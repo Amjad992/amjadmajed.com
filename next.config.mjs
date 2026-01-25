418b8aebd1d417a9923df658715f56e76d83a352
Amjad992 <Amjad@desolint.com>
2026-01-26 00:58:37 +0500

    Feat: add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/amjad992/',
+        permanent: true,
+      },
+      {
+        source: '/fb',
+        destination: 'https://www.facebook.com/Amjad992',
+        permanent: true,
+      },
+      {
+        source: '/facebook',
+        destination: 'https://www.facebook.com/Amjad992',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/Amjad992',
+        permanent: true,
+      },
+      {
+        source: '/insta',
+        destination: 'https://www.instagram.com/Amjad992/',
+        permanent: true,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://www.instagram.com/Amjad992/',
+        permanent: true,
+      },
+      {
+        source: '/git',
+        destination: 'https://github.com/Amjad992',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/Amjad992',
+        permanent: true,
+      },
+      {
+        source: '/upwork',
+        destination: 'https://www.upwork.com/fl/amjad',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
